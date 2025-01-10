c73cd4cc495b8a0803fa9f0d0f56565b7be2a520
dev1ous <60881111+dev1ous@users.noreply.github.com>
2025-01-10 10:15:30 +0100

    Update single_header_template.hpp

--- single_header_template.hpp
+++ single_header_template.hpp
@@ -63,3 +63,4 @@
 #include "boost/unordered/unordered_flat_map.hpp"
 #include "boost/unordered/unordered_node_set.hpp"
 #include "boost/unordered/unordered_node_map.hpp"
+#include <boost/unordered/concurrent_node_map.hpp>
